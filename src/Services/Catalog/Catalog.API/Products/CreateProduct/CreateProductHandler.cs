ee6f7ee3e7d9ba1f400e850d7b0a60ea5dcc2de0
Rahul Dey <rahuldey9623@gmail.com>
2026-07-24 17:19:28 +0530

    Added logging in create product handler

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using BuildingBlock.CQRS;
 using Catalog.API.Models;
 using Marten;
@@ -13,10 +14,15 @@ public record CreateProductCommand(
 
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler(
+    IDocumentSession session,
+    ILogger<CreateProductCommandHandler> logger)
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken ct)
     {
+        logger.LogInformation("Start executing create product command handler. Command: {@Command}", command);
+
         // Create product entity from the command object
         var product = new Product
         {
@@ -26,10 +32,13 @@ internal class CreateProductCommandHandler(IDocumentSession session) : ICommandH
             ImageUrl = command.ImageUrl,
             Price = command.Price
         };
+
         // Save this product into database
         session.Store(product);
         await session.SaveChangesAsync(ct);
 
+        logger.LogInformation("Product with id {@ProductId} created successfully.", product.Id);
+        logger.LogInformation("Completed create product command handler. Product: {@Product}", JsonSerializer.Serialize(product));
         // Return the result
         return new CreateProductResult(product.Id);
     }
