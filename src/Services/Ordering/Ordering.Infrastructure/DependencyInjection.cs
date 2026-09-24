8f80d1316d7943d6a77a79ecfded8c743f28a587
Rahul Dey <rahuldey9623@gmail.com>
2026-09-24 07:48:06 +0530

    Add dependency injections in each ordering layer

---
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        // Add infrastructure service dependencies
+
+        return services;
+    }
+}
