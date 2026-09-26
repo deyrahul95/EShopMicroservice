278f0b08062415134b0282102e8cf430e60f6cbf
Rahul Dey <rahuldey9623@gmail.com>
2026-09-26 13:27:51 +0530

    Added domain value objects

--- src/Services/Ordering/Ordering.Domain/Models/Product.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -1,8 +1,9 @@
 using Ordering.Domain.Abstractions;
+using Ordering.Domain.ValueObjects;
 
 namespace Ordering.Domain.Models;
 
-public class Product : Entity<Guid>
+public class Product : Entity<ProductId>
 {
     public string Name { get; private set; } = string.Empty;
     public decimal Price { get; private set; }
