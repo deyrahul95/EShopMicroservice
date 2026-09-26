55fda208685a1c120f87cd5a7635cb8ee5b2fc4a
Rahul Dey <rahuldey9623@gmail.com>
2026-09-26 20:15:19 +0530

    Added application db context and configuration

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Infrastructure.Data.Configurations;
+
+public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        builder.HasKey(o => o.Id);
+        builder.Property(o => o.Id)
+            .HasConversion(orderItemId => orderItemId.Value,
+            dbId => OrderItemId.Of(dbId));
+    }
+}
